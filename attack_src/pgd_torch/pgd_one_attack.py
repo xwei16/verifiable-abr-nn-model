49b7b0691547a5dad0faa3d877881ddb2c95701e
wy-go <wanyu2@illinois.edu>
2025-04-24 20:14:19 -0500

    Clean code

--- attack_src/pgd_torch/pgd_one_attack.py
+++ attack_src/pgd_torch/pgd_one_attack.py
@@ -23,6 +23,7 @@ import ppo2 as network
 
 
 MODEL_PATH   = "../../pensieve_rl_model/nn_model_ep_155400.pth"
+# CSV_PATH     = "../../src/pensieve_big_testing_data.csv"
 CSV_PATH     = "../../src/filtered_good_testing_data.csv"
 SPEC_PATH    = "../../src/qoe_spec.json"
 OUT_CSV      = "pgd_attacked_torch_data.csv"
@@ -37,7 +38,6 @@ ALPHA = 0.001                       # step size
 STEPS = 40                          # number of PGD steps
 TARGET_CLASS = 0                    # target class [0: 300 kbps, 1: 1000 kbps]
 M = 0                               # number of spec
-# DEVICE = torch.device("cpu")
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # PPO2 parameters
@@ -45,7 +45,7 @@ S_DIM = [6, 8]
 A_DIM = 6
 ACTOR_LR_RATE = 1e-4
 
-
+# TODO: hardcoded
 def row_to_state(r, spec):
     """Convert one pandas row to Pensieve state (6×8)."""
     s = np.zeros((6, 8), dtype=np.float32)
@@ -55,16 +55,16 @@ def row_to_state(r, spec):
     s[3] = r[[f"Last{i}_downloadtime" for i in range(8, 0, -1)]].values
     s[4, :6] = r[[f"chunksize{i}"        for i in range(1, 7)]].values
     s[5, 0] = r["Chunks_left"]
-    # print(len(spec))
-    # print(spec)
-    chosen = random.choice(spec)
-    # print(len(chosen))
-    # print(chosen)
+
+    chosen = spec[random.randint(0, len(spec) - 1)]   
     s[3, 7] = random.uniform(
         chosen["Last1_downloadtime_l"],
         chosen["Last1_downloadtime_u"]
     )
-    return s
+    low = chosen["Last1_downloadtime_l"]
+    high = chosen["Last1_downloadtime_u"]
+
+    return s, low, high
 
 
 ## Load data
@@ -73,16 +73,7 @@ df   = pd.read_csv(CSV_PATH)
 raw  = json.load(open(SPEC_PATH))
 spec  = raw if isinstance(raw, list) else [raw]
 M    = len(spec)
-
-low  = np.array([spec[i][f"{k}_l"] for k in FEATURES for i in range(M)],
-                dtype=np.float32)
-high = np.array([spec[i][f"{k}_u"] for k in FEATURES for i in range(M)],
-                dtype=np.float32)
-
-#TODO: [3, 7] is the index of Last1_downloadtime
 N = len(df)
-states_np = np.stack([row_to_state(r, spec) for _, r in df.iterrows()])  # Shape: (N, 6, 8)
-orig_feats = states_np[:, 3, 7].astype(np.float32)
 
 if os.path.exists(MODEL_PATH):
     # TODO: load parameters from file
@@ -100,17 +91,13 @@ else:
 
 ## PGD attack
 
-adv_feats = np.empty_like(orig_feats)
+adv_feats = np.zeros((N, 1))
 
-# model.eval()
+for i in tqdm(range(N), desc="PGD attack"):
+    state0, low_bound, high_bound = row_to_state(df.iloc[i], spec=spec)
+    x0 = state0[3, 7].astype(np.float32)
 
-
-for i in tqdm(range(N)):
-
-    # state0 = torch.tensor(states_np[i:i+1], device=DEVICE).cpu().detach().numpy()
-    state0 = states_np[i:i+1]
-    x0 = orig_feats[i]
-    
+    state0 = np.expand_dims(state0, axis=0)
     pred = model.predict(state0)
     print(f"pred: {pred}")
     pred = np.argmax(pred)
@@ -123,9 +110,9 @@ for i in tqdm(range(N)):
     x_adv = x0
     
     for _ in range(STEPS):
-        state_cur = state0.clone().detach().requires_grad_(True).flatten()
-        state_cur_flat = state_cur.view(-1)
-        state_cur_flat[ATTACK_IDXS[0]] = float(x_adv)
+        state_cur = state0.flatten()
+        state_cur[ATTACK_IDXS[0]] = float(x_adv)
+        state_cur = state_cur.reshape(state0.shape)
         
         output = np.argmax(model.predict(state_cur))
         target = torch.tensor([TARGET_CLASS], device=DEVICE)
@@ -136,16 +123,7 @@ for i in tqdm(range(N)):
         x_perturbation = ALPHA * torch.sign(grad)
         x_adv += x_perturbation
         x_adv = torch.clamp(x_adv, x0 - EPS, x0 + EPS)
-        x_adv = torch.clamp(x_adv, low, high)
-
-        if i == 0:
-            print(f"x_adv: {x_adv}")
-            print(f"x0: {x0}")
-            print(f"x_perturbation: {x_perturbation}")
-            print(f"grad: {grad}")
-            print(f"output: {output}")
-            print(f"target: {target}")
-            print(f"loss: {loss}")
+        x_adv = torch.clamp(x_adv, low_bound, high_bound)
     
     adv_feats[i] = x_adv
 
@@ -160,26 +138,19 @@ print(f"Adversarial CSV written →  {OUT_CSV}")
 
 ## Verify attack effectiveness
 
-print("\n▶  Verifying attack effectiveness …")
-
 success = unchanged = higher = 0
 orig_preds, adv_preds = [], []
 
 with torch.no_grad():
-    for i in range(N):
-        # state_orig = states_np[i:i+1]
-        # state_adv = state_orig.clone().detach().requires_grad_(True).flatten()
-        # state_adv_flat = state_adv.view(-1)
-        # state_adv_flat[ATTACK_IDXS[0]] = float(x_adv)
-
-        state_orig = states_np[i:i+1]
+    for i in tqdm(range(N), desc="Verifying attack effectiveness"):
+        state_orig, _, _ = row_to_state(df.iloc[i], spec=spec)
         state_orig_flat = state_orig.flatten()
         state_adv = state_orig_flat.copy()
-        state_adv[ATTACK_IDXS[0]] = adv_feats[i, 0]
+        state_adv[ATTACK_IDXS[0]] = adv_feats[i]
         state_adv = state_adv.reshape(state_orig.shape)
 
-        # state_orig = torch.tensor(state_orig, device=DEVICE).requires_grad_(True)
-        # state_adv = torch.tensor(state_adv, device=DEVICE).requires_grad_(True)
+        state_orig = np.expand_dims(state_orig, axis=0)
+        state_adv = np.expand_dims(state_adv, axis=0)
 
         p_orig = np.argmax(model.predict(state_orig))
         p_adv = np.argmax(model.predict(state_adv))
